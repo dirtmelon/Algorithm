c8f657bbefaa0feaebb75db4210fb6dbb17dc4d6
dirtmelon <0xffdirtmelon@gmail.com>
2019-04-18 12:30:17 +0800

    Manacher 算法

--- LeetCode/5.LongestPalindromicSubstring.swift
+++ LeetCode/5.LongestPalindromicSubstring.swift
@@ -40,3 +40,36 @@ class Solution {
 	}
 }
 /// https://segmentfault.com/a/1190000003914228 Manacher 算法
+//class Solution {
+//	func longestPalindrome(_ s: String) -> String {
+//		guard s.count >= 2 else { return s }
+//		let characters = Array("#" + Array(s).map { String($0)}.joined(separator: "#") + "#")
+//		var rl: [Int] = Array(repeating: 0, count: characters.count)
+//		var tmpMaxRight = 0
+//		var tmpPos = 0
+//		var maxRight = 0
+//		var pos = 0
+//		var maxLen = 0
+//		for i in 0..<characters.count {
+//			if i < tmpMaxRight {
+//				rl[i] = min(rl[2 * tmpPos - i], tmpMaxRight - i)
+//			} else {
+//				rl[i] = 1
+//			}
+//			while i - rl[i] >= 0 && i + rl[i] < characters.count && characters[i - rl[i]] == characters[i + rl[i]] {
+//				rl[i] += 1
+//			}
+//			if rl[i] + i - 1 > tmpMaxRight {
+//				tmpMaxRight = rl[i] + i - 1
+//				tmpPos = i
+//			}
+//			if maxLen < rl[i] {
+//				maxRight = tmpMaxRight
+//				pos = tmpPos
+//				maxLen = rl[i]
+//			}
+//		}
+//		let start = pos - (maxRight - pos)
+//		return String(characters[start..<maxRight]).replacingOccurrences(of: "#", with: "")
+//	}
+//}
