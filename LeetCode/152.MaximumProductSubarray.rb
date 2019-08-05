d4e01ed6ffaf3999f9b37ea70f25e4b2485a806e
dirtmelon <0xffdirtmelon@gmail.com>
2019-08-05 22:38:34 +0800

    152. Maximum Product Subarray

---
+++ LeetCode/152.MaximumProductSubarray.rb
@@ -0,0 +1,20 @@
+# @param {Integer[]} nums
+# @return {Integer}
+def max_product(nums)
+  # return 0 if nums.size == 0
+  result = nums[0]
+  max_result = nums[0]
+  min_result = nums[0]
+  index = 1
+  while index < nums.size
+    number = nums[index]
+    max_result, min_result = min_result, max_result if number < 0
+    max_result = [max_result * number, number].max
+    min_result = [min_result * number, number].min
+    index += 1
+    result = [max_result, result].max
+  end
+  return result
+end
+
+p max_product([])
