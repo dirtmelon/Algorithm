5720f0151dd4f7d042fbb9787943ac86cc2b16f7
dirtmelon <0xffdirtmelon@gmail.com>
2019-08-16 15:26:22 +0800

    66 Plus One

---
+++ LeetCode/66.PlusOne.rb
@@ -0,0 +1,22 @@
+# @param {Integer[]} digits
+# @return {Integer[]}
+def plus_one(digits)
+  next_value = 1
+  index = digits.size - 1
+  while index >= 0 || next_value != 0
+    if index >= 0
+      digits[index] += next_value
+      if digits[index] == 10
+        digits[index] = 0
+      else
+        return digits
+      end
+    else
+      digits.unshift 1
+      return digits
+    end
+    index -= 1
+  end
+end
+
+p plus_one([1,2,3])
