899b0d34fbd2b37ed941f84e8b308ee61e7a30ab
dirtmelon <0xffdirtmelon@gmail.com>
2019-08-17 23:57:07 +0800

    不直接使用立方运算符

--- LeetCode/372.SuperPow.rb
+++ LeetCode/372.SuperPow.rb
@@ -6,9 +6,21 @@ def super_pow(a, b)
   mod = 1337
   pre_base = a % mod
   b.reverse_each { |num|
-    cur_result = pre_base ** num % mod
+    cur_result = pow(pre_base, num) % mod
     result = (result * cur_result % mod) % mod
-    pre_base = (pre_base ** 10) % mod
+    pre_base = pow(pre_base, 10) % mod
   }
   return result
+end
+
+def pow(pre_base, num)
+  mod = 1337
+  cur_base = pre_base % mod
+  res = 1
+  while num > 0
+    res = (res * cur_base) % mod if num & 1 == 1
+    cur_base = cur_base * cur_base % 1337
+    num = num >> 1
+  end
+  return res
 end
