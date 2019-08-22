5082c4be3d491c8677d7bdf260c87778e4bf0a95
dirtmelon <0xffdirtmelon@gmail.com>
2019-08-22 22:34:56 +0800

    401.Binary Watch

--- LeetCode/401.BinaryWatch.rb
+++ LeetCode/401.BinaryWatch.rb
@@ -24,3 +24,15 @@ def read_binary_watch(num)
   end
   return result
 end
+
+# @param {Integer} num
+# @return {String[]}
+# def read_binary_watch(num)
+#   res = []
+#   (0..11).each do |h|
+#     (0..59).each do |s|
+#       res.push "#{h}:#{s.to_s.rjust(2, "0")}" if (h.to_s(2)+s.to_s(2)).count('1') == num
+#     end
+#   end
+#   res
+# end
