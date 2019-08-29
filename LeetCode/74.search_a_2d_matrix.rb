dead94949ad1373aa727980b5011533bf62f0361
dirtmelon <0xffdirtmelon@gmail.com>
2019-08-29 21:36:22 +0800

    74. Search a 2D Matrix

---
+++ LeetCode/74.search_a_2d_matrix.rb
@@ -0,0 +1,45 @@
+# @param {Integer[][]} matrix
+# @param {Integer} target
+# @return {Boolean}
+def search_matrix(matrix, target)
+  return false if matrix.empty? || matrix[0].empty?
+
+  nums_size = matrix[0].size
+  low = 0
+  high = matrix.size - 1
+  index = -1
+  while low <= high
+    mid = low + (high - low) / 2
+    if matrix[mid][0] <= target && matrix[mid][nums_size - 1] >= target
+      index = mid
+      break
+    elsif matrix[mid][0] > target
+      high = mid - 1
+    else
+      low = mid + 1
+    end
+  end
+  return false if index == -1
+
+  nums = matrix[index]
+  low = 0
+  high = nums_size - 1
+  while low <= high
+    mid = low + (high - low) / 2
+    return true if nums[mid] == target
+
+    if nums[mid] > target
+      high = mid - 1
+    else
+      low = mid + 1
+    end
+  end
+  false
+end
+
+p search_matrix([
+                  [1, 3, 5, 7],
+                  [10, 11, 16, 20],
+                  [23, 30, 34, 50]
+                ],
+                13)
