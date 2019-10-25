27ec7eb0963dee4b84fe24c6b0c656ce5f001c04
dirtmelon <0xffdirtmelon@gmail.com>
2019-10-26 00:54:04 +0800

    62. Unique Paths

--- LeetCode/62.unique-paths.swift
+++ LeetCode/62.unique-paths.swift
@@ -7,20 +7,14 @@
 // @lc code=start
 class Solution {
     func uniquePaths(_ m: Int, _ n: Int) -> Int {
-        guard m > 1 && n > 1 else { return 1}
-        var dp: [[Int]] = Array(repeating: Array(repeating: 0, count: m), count: n)
-        for index in 0..<m {
-            dp[0][index] = 1
-        }
-        for index in 0..<n {
-            dp[index][0] = 1
-        }
+        var dp: [Int] = Array(repeating: 1, count: m)
+        
         for row in 1..<n {
             for column in 1..<m {
-                dp[row][column] = dp[row - 1][column] + dp[row][column - 1]
+                dp[column] += dp[column - 1]
             }
         }
-        return dp[n - 1][m - 1]
+        return dp[m - 1]
     }
 }
 // @lc code=end
